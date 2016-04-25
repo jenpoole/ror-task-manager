2839daa17f9482cc6bd5303b6a12b16d8fd90f5f
Jen <contact.vandepool@gmail.com>
2016-04-25 02:28:33 +0000

    complete a task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425014543) do
+ActiveRecord::Schema.define(version: 20160425015856) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20160425014543) do
     t.string   "content"
     t.string   "project"
     t.string   "references"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "project_id"
+    t.datetime "completed_at"
   end
 
 end
